ddf025eaafbefc248bf11b3ff079c931906011bd
James Anderson <james.r.anderson@utah.edu>
2024-06-05 13:01:46 -0700

    Typing information for stos maps

--- nornir_volumemodel/model/stos_map.py
+++ nornir_volumemodel/model/stos_map.py
@@ -1,47 +1,56 @@
-'''
+"""
 Created on Apr 27, 2016
 
 @author: u0490822
-'''
+"""
 
-class stos_map(object):
-    '''
-    classdocs
-    '''
-     
+
+class stos_map:
+    """
+    Map of slice-to-slice transformations
+    """
+    _name: str
+    _center_section: int
+    _mappedToControl: dict[int, set[int]]
+
     @property
-    def CenterSection(self):
+    def CenterSection(self) -> int:
+        """
+        Section that every other section eventually maps onto
+        """
         return self._center_section
-    
+
     @CenterSection.setter
-    def CenterSection(self, val):
+    def CenterSection(self, val: int | str):
         self._center_section = int(val)
-        
-    def AddMapping(self, ControlSection, MappedSection):
-        if not MappedSection in self._mappedToControl:
-            self._mappedToControl[MappedSection] = set([ControlSection])
+
+    def AddMapping(self, ControlSection: int, MappedSection: int):
+        if MappedSection not in self._mappedToControl:
+            self._mappedToControl[MappedSection] = {ControlSection}
         else:
             self._mappedToControl[MappedSection].add(ControlSection)
 
-    def __init__(self, Name, CenterSection):
-        '''
+    def __init__(self, Name: str, CenterSection: int):
+        """
         Constructor
-        '''
+        """
         self._name = Name
         self._center_section = CenterSection
         self._mappedToControl = {}
-        
-        
+
+
 class stos_mapping(object):
-    
+    _control: int
+    _mapped: int
+
     @property
-    def Control(self):
+    def Control(self) -> int:
         return self._control
-    
+
     @property
-    def Mapped(self):
+    def Mapped(self) -> int:
         return self._mapped
-    
-    def __init__(self, ControlSection, MappedSection):
+
+    def __init__(self, ControlSection: int | str, MappedSection: int | str):
         self._control = int(ControlSection)
         self._mapped = int(MappedSection)
